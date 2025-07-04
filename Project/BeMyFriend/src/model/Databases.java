bf9565341eafae6a102a283fdae78bbc867de2d2
LEEJOEUN-01 <joeun1005@naver.com>
2025-07-04 15:32:39 +0900

    feat: 사람, 학생 객체 생성 및 모델 제작

--- Project/BeMyFriend/src/model/Databases.java
+++ Project/BeMyFriend/src/model/Databases.java
@@ -1,5 +1,14 @@
 package model;
 
 public class Databases {
-
+	private static Student [] students;
+	
+	static {
+		// 학생들 DB
+		students = new Student[] {};
+	}
+	
+	Student [] getStudents() {
+		return students;
+	}
 }
---
+++ Project/BeMyFriend/src/model/Student.java
@@ -0,0 +1,8 @@
+package model;
+
+public class Student extends Person {
+	String mbti;
+	String [] hates;
+	String [] favorites;
+	String [] food;
+}
