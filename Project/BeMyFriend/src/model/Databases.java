34df1e06c9cb5ad30cb8a8adba800deccbcead32
LimYuJin <erinzcoding@gmail.com>
2025-07-04 15:43:10 +0900

    feat: 이름 받아서 사용자 검색 기능 구현중

--- Project/BeMyFriend/src/model/Databases.java
+++ Project/BeMyFriend/src/model/Databases.java
@@ -1,5 +1,16 @@
 package model;
 
 public class Databases {
-
+	// 현재 사용자 정보
+	private static Student currentUser;
+	
+	// 현재 사용자 설정
+	public static void setCurrentUser(Student user) {
+		currentUser = user;
+	}
+	
+	// 현재 사용자 가져오기
+	public static Student getCurrentUser() {
+		return currentUser;
+	}
 }
